52db013586c10394bd37b08fdf7118c7c3a8c341
Sergio Bateca <sbtk87@hotmail.com>
2022-08-09 20:23:04 -0500

    Feature: implementing functions to manage beverages in the frontend

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@
 function postOrder(order) {
 
     fetch('http://127.0.0.1:5000/order/', {
-        method: 'POST',
-        body: JSON.stringify(order),
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-        },
-    })
+            method: 'POST',
+            body: JSON.stringify(order),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+        })
         .then(res => res.json())
         .then(res => showNotification());
 
@@ -36,8 +36,12 @@ orderForm.submit(event => {
  */
 function getOrderData() {
     let ingredients = [];
-    $.each($("input[name='ingredients']:checked"), function (el) {
+    let beverages = [];
+    $.each($("input[name='ingredients']:checked"), function(el) {
         ingredients.push($(this).val());
+    });
+    $.each($("input[name='beverages']:checked"), function(el) {
+        beverages.push($(this).val());
     });
 
     return {
@@ -46,7 +50,8 @@ function getOrderData() {
         client_address: $("input[name='address']").val(),
         client_phone: $("input[name='phone']").val(),
         size_id: $("input[name='size']:checked").val(),
-        ingredients
+        ingredients,
+        beverages
     };
 }
 
@@ -72,6 +77,18 @@ function fetchIngredients() {
         });
 }
 
+
+function fetchBeverages() {
+    fetch('http://127.0.0.1:5000/beverage/')
+        .then(response => response.json())
+        .then(beverages => {
+            let rows = beverages.map(element => createBeverageTemplate(element));
+            let table = $("#beverages tbody");
+            table.append(rows);
+        });
+}
+
+
 function fetchOrderSizes() {
     fetch('http://127.0.0.1:5000/size/')
         .then(response => response.json())
@@ -87,6 +104,11 @@ function createIngredientTemplate(ingredient) {
     return Mustache.render(template, ingredient);
 }
 
+function createBeverageTemplate(beverage) {
+    let template = $("#beverages-template")[0].innerHTML;
+    return Mustache.render(template, beverage);
+}
+
 function createSizeTemplate(size) {
     let template = $("#sizes-template")[0].innerHTML;
     return Mustache.render(template, size);
@@ -94,6 +116,7 @@ function createSizeTemplate(size) {
 
 function loadInformation() {
     fetchIngredients();
+    fetchBeverages();
     fetchOrderSizes();
 }
 
